8504dfbd2346e02e2a78e68b94ca6f5b38609a73
Alex Lazareva <alex.lazareva25@yandex.ru>
2018-09-09 23:23:59 +0300

    create maps clusters

--- src/js/modules/yandexMapApi.js
+++ src/js/modules/yandexMapApi.js
@@ -1,5 +1,5 @@
 export default class {
-    constructor () {};
+    constructor () {}
 
     initMap() {
         return new Promise ((resolve, reject) => ymaps.ready(resolve)
@@ -8,7 +8,37 @@ export default class {
                     center: [55.78874, 49.12214],
                     zoom: 12
                 });
+
+                this.cluster = new ymaps.Clusterer({
+                    clusterDisabledClickZoom: true,
+                    clusterOpenBalloonOnClick: true,
+                    clusterBalloonContentLayout: 'cluster#balloonCarousel'
+                });
+
+                this.map.geoObjects.add(this.cluster);
+
+                return this.map;
             })
         );
+    }
+
+    async getMapPosition(e) {
+        const coords = e.get('coords');
+        const geocode = await ymaps.geocode(coords);
+        const address = geocode.geoObjects.get(0).properties.get('text');
+
+        return {
+            address,
+            coords
+        }
+    }
+
+    createPlacemark(pos) {
+        const myPlacemark = new ymaps.Placemark({
+            hintContent: pos.address,
+            balloonContent: `Мы кликнули на ${pos.address}`
+        });
+
+        this.cluster.add(myPlacemark);
     }
 }
